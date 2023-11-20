9de8aa04efc5fd31541f9aa8df63a078fb059599
Tyuiu.DolgushinVA@outlook.com <vadimdolgushin9@gmail.com>
2023-11-20 18:51:52 +0500

    Task4

---
+++ Tyuiu.DolgushinVA.Sprint5.Task4.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DolgushinVA.Sprint5.Task4.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DolgushinVA.Sprint5.Task4.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8d34297-b628-4a05-8053-d379bab278bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.DolgushinVA.Sprint5.Task4.V1/Program.cs
+++ Tyuiu.DolgushinVA.Sprint5.Task4.V1/Program.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.DolgushinVA.Sprint5.Task3.V24.Lib;
-namespace Tyuiu.DolgushinVA.Sprint5.Task3.V24
+using System.IO;
+using Tyuiu.DolgushinVA.Sprint5.Task4.V1.Lib;
+namespace Tyuiu.DolgushinVA.Sprint5.Task4.V1
 {
     class Program
     {
@@ -15,34 +16,27 @@ namespace Tyuiu.DolgushinVA.Sprint5.Task3.V24
             Console.Title = "Спринт #5 | Выполнил: Долгушин В. А. | ИИПб-23-3";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Задание #3                                                              *");
-            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
             Console.WriteLine("* Выполнил: Долгушин Вадим Алексеевич | ИИПб-23-3                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычисляет значение выражения при x = 3.     *");
-            Console.WriteLine("* Результат сохранить в текстовый файл OutPutFileTask3.bin и вывести      *");
-            Console.WriteLine("* на консоль.                                                             *");
+            Console.WriteLine("* Написать программу, которая берёт значение из InPutDataFileTask4V0.txt  *");
+            Console.WriteLine("* и подставляет вместо Х в формулу. Результат вывести на консоль.         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int x = 3;
-            Console.WriteLine("x = " + x);
+            string path = @"C:\DataSprint5\InPutDataFileTask4V1.txt";
+            Console.WriteLine("Данные находятся в файле:" + path);
+            
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            double y = 6.1 * Math.Pow(x, 3) + 0.23 * Math.Pow(x, 2) + 1.04 * x;
-            y = Math.Round(y, 3);
-            Console.WriteLine("Ответ: " + y);
-
-            Console.WriteLine("***************************************************************************");
-
-            string res = ds.SaveToFileTextData(x);
-            Console.WriteLine("Фaйл: " + res);
-            Console.WriteLine("Coздан!");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
             Console.ReadKey();
         }
     }
